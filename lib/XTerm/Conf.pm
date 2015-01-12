b5ea20dc37ed5ea4388d0636ef9b04e493e6a7fa
Slaven Rezic <slaven@rezic.de>
2015-01-12 23:29:44 +0100

    version 0.10

--- lib/XTerm/Conf.pm
+++ lib/XTerm/Conf.pm
@@ -21,7 +21,7 @@ use 5.006; # qr, autovivified filehandles
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-$VERSION = '0.09_51';
+$VERSION = '0.10_00';
 
 require Exporter;
 @ISA = qw(Exporter);
